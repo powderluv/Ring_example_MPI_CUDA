d32333a3309e620cc56331f631f4c68eeacca376
weilewei <lokwei9@gmail.com>
2019-10-24 23:26:30 -0400

    WIP the program seems working

--- G2_ring.cpp
+++ G2_ring.cpp
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
 //        exit(-1);
 //    }
 
-    int n_recevied = 0;
+    int iter = 0;
 
     std::vector<int> G2s;
     G2s.reserve(mpi_size);
@@ -38,25 +38,36 @@ int main(int argc, char **argv) {
     int flag = 0;
     int tag = rank;
     int count;
+    bool sent = false;
     MPI_Status status;
     MPI_Request request;
 
-    while(n_recevied <= mpi_size)
-    {
-        MPI_Isend(&G2s[rank], 1, MPI_INT, (rank + 1) % mpi_size, tag, MPI_COMM_WORLD, &request);
 
-        while(!flag)
-        {
+    while(iter < mpi_size) {
+
+        if (!sent) {
+            MPI_Isend(&G2s[rank], 1, MPI_INT, (rank + 1) % mpi_size, rank, MPI_COMM_WORLD, &request);
+            std::cout << "Rank " << rank << " sent!" << "\n";
+            sent = true;
+        }
+
+        while (flag == 0) {
             MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
         }
-        MPI_Get_count(&status, MPI_INT, &count);
-        MPI_Recv(&G2s[status.MPI_SOURCE], count, MPI_INT, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
 
-        std::cout << "I am rank " << rank << " received " << G2s[status.MPI_SOURCE] << " from rank " << status.MPI_TAG << "\n";
+        if (flag) {
+            MPI_Recv(&G2s[status.MPI_TAG], 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            std::cout << "I am rank " << rank << " received " << G2s[status.MPI_TAG] << " from rank " << status.MPI_SOURCE
+                      << "\n";
+            flag = 0;
+        }
+
+        std::cout << "I am rank " << rank << " sending " << G2s[status.MPI_TAG] << " to rank " << (rank + 1) % mpi_size
+                  << "\n";
+
         if(status.MPI_TAG != rank)
         {
-            MPI_Isend(&G2s[status.MPI_SOURCE], 1, MPI_INT, (rank + 1) % mpi_size, status.MPI_TAG, MPI_COMM_WORLD, &request);
-
+            MPI_Isend(&G2s[status.MPI_TAG], 1, MPI_INT, (rank + 1) % mpi_size, status.MPI_TAG, MPI_COMM_WORLD, &request);
             // updateG4();
         }
         else
@@ -64,8 +75,10 @@ int main(int argc, char **argv) {
             // do nothing
         }
 
-        n_recevied++;
+
+        iter++;
     }
+
 //    updateG4();
 
     // updateG4 local
