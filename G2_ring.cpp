c42ad0abd08b8a83a91e7d0e40594d87e455b950
weilewei <lokwei9@gmail.com>
2019-10-25 00:10:03 -0400

    add comments and remove unnecessary code

--- G2_ring.cpp
+++ G2_ring.cpp
@@ -6,119 +6,83 @@
 #include <vector>
 #include <string>
 
-#include "util.hpp"
-
-float* allc_G(float* G2);
-
-//void updateG4();
+ #include "util.hpp"
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
     int rank, mpi_size;
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//    if (mpi_size != 2) {
-//        std::cout << "Run with two ranks.";
-//        MPI_Abort(MPI_COMM_WORLD, -1);
-//        exit(-1);
-//    }
 
-    int iter = 0;
 
-    std::vector<int> G2s;
-    G2s.reserve(mpi_size);
 
-//    for(int i = 0; i<mpi_size; i++)
-//    {
-////        alloc_d(4, &G2s[i]);
-//    }
+    // each G2 say has 4 elements
+    int n_elems = 4;
+    float* G2;
+    // allocate a sequence of buffers for all G2s
+    std::vector<float*> G2s;
+    alloc_d(n_elems, &G2);
+    for(int i = 0; i<mpi_size; i++)
+    {
+        G2s.emplace_back(G2);
+    }
 
-    G2s[rank] = rank;
+    // generate G2
+    // computeG();
 
     int flag = 0;
     int tag = rank;
-    int count;
     bool sent = false;
     MPI_Status status;
     MPI_Request request;
 
-
+    int iter = 0;
     while(iter < mpi_size) {
 
+        // do once at the beginning, sent out local G2
         if (!sent) {
-            MPI_Isend(&G2s[rank], 1, MPI_INT, (rank + 1) % mpi_size, rank, MPI_COMM_WORLD, &request);
-            std::cout << "Rank " << rank << " sent!" << "\n";
+            // each G2 is tagged with its birth rank
+            MPI_Isend(G2s[rank], n_elems, MPI_FLOAT, (rank + 1) % mpi_size, rank, MPI_COMM_WORLD, &request);
+            std::cout << "Rank " << rank << " sent my G2!" << "\n";
             sent = true;
         }
 
+        // probe any available incoming G2
         while (flag == 0) {
             MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
         }
 
+        // we found one available G2 from left neighbor, let's place it into corresponding buffer position
         if (flag) {
-            MPI_Recv(&G2s[status.MPI_TAG], 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            std::cout << "I am rank " << rank << " received " << G2s[status.MPI_TAG] << " from rank " << status.MPI_SOURCE
-                      << "\n";
+            // birth rank (tag) <-> position of sequence buffer
+            MPI_Recv(G2s[status.MPI_TAG], n_elems, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            std::cout << "Rank " << rank << " received G2 [ " << status.MPI_TAG << " ] from rank " << status.MPI_SOURCE << "\n";
             flag = 0;
         }
 
-        std::cout << "I am rank " << rank << " sending " << G2s[status.MPI_TAG] << " to rank " << (rank + 1) % mpi_size
-                  << "\n";
-
         if(status.MPI_TAG != rank)
         {
-            MPI_Isend(&G2s[status.MPI_TAG], 1, MPI_INT, (rank + 1) % mpi_size, status.MPI_TAG, MPI_COMM_WORLD, &request);
-            // updateG4();
+            std::cout << "Rank " << rank << " is sending G2 [ " << status.MPI_TAG << " ] to rank " << (rank + 1) % mpi_size << "\n";
+            // forward G2 to my right neighbor using non-blocking Isend
+            MPI_Isend(G2s[status.MPI_TAG], 1, MPI_FLOAT, (rank + 1) % mpi_size, status.MPI_TAG, MPI_COMM_WORLD, &request);
         }
         else
         {
+            std::cout << "Rank " << rank << " will not send G2 [ " << status.MPI_TAG << " ] to anywhere! \n";
+            // this G2 was originally from me, it has travel around the ring and can be retired
             // do nothing
         }
-
 
         iter++;
     }
 
-//    updateG4();
+    for(int i = 0; i < mpi_size; i++)
+    {
+        // every G2 do your work to complete one cell of G4
+        // updateG4(G2s[i]);
+    }
 
-    // updateG4 local
-    // send myself to right
-
-
-//    while(n_recevied <= mpi_size - 1)
-//    {
-//        // if there is a G2 waiting
-//        // then receive it, then updateG4
-//        // flip the flag in the queue received
-//        // send it to my right
-//
-//        n_recevied++;
-//    }
+    // MPI gather to complete full final G4
 
     MPI_Finalize();
 }
-
-float* allc_G(float* G2)
-{
-    alloc_d(4, &G2);
-}
-
-
-//if (rank != 0) {
-//MPI_Recv(left, 1, MPI_FLOAT, rank - 1, 0,
-//MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//printf("Process %d received token %d from process %d\n",
-//rank, 42, rank - 1);
-//} else {
-//// compute G2 if process is 0
-//}
-//MPI_Send(G2, 1, MPI_FLOAT, (rank + 1) % mpi_size,
-//0, MPI_COMM_WORLD);
-//
-//// Now process 0 can receive from the last process.
-//if (rank == 0) {
-//MPI_Recv(left, 1, MPI_FLOAT, mpi_size - 1, 0,
-//MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-//printf("Process %d received token %d from process %d\n",
-//rank, 42, mpi_size - 1);
-//}
