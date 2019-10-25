145c527ff5589438eafb8aef119ff2163fdef588
weilewei <lokwei9@gmail.com>
2019-10-25 00:17:01 -0400

    perfecting codes

--- G2_ring.cpp
+++ G2_ring.cpp
@@ -6,7 +6,11 @@
 #include <vector>
 #include <string>
 
- #include "util.hpp"
+#include "util.hpp"
+
+void computeG(){}
+
+void updateG4(){}
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
@@ -14,21 +18,20 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-
-
     // each G2 say has 4 elements
     int n_elems = 4;
     float* G2;
     // allocate a sequence of buffers for all G2s
     std::vector<float*> G2s;
+    // G2 is currently empty
     alloc_d(n_elems, &G2);
     for(int i = 0; i<mpi_size; i++)
     {
         G2s.emplace_back(G2);
     }
 
-    // generate G2
-    // computeG();
+    // generate G2 and fill some value in
+    computeG();
 
     int flag = 0;
     int tag = rank;
@@ -39,11 +42,11 @@ int main(int argc, char **argv) {
     int iter = 0;
     while(iter < mpi_size) {
 
-        // do once at the beginning, sent out local G2
+        // do once at the beginning, sent local G2 to right neighbour
         if (!sent) {
             // each G2 is tagged with its birth rank
             MPI_Isend(G2s[rank], n_elems, MPI_FLOAT, (rank + 1) % mpi_size, rank, MPI_COMM_WORLD, &request);
-            std::cout << "Rank " << rank << " sent my G2!" << "\n";
+            std::cout << "Rank " << rank << " sent its G2!" << "\n";
             sent = true;
         }
 
@@ -79,7 +82,7 @@ int main(int argc, char **argv) {
     for(int i = 0; i < mpi_size; i++)
     {
         // every G2 do your work to complete one cell of G4
-        // updateG4(G2s[i]);
+        updateG4(); // G2s[i];
     }
 
     // MPI gather to complete full final G4
