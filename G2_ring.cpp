5f1b103c822616f87ee735aedbce83babc0b052b
weilewei <lokwei9@gmail.com>
2019-11-01 16:24:27 -0400

    clean up printf and timing info

--- G2_ring.cpp
+++ G2_ring.cpp
@@ -44,6 +44,12 @@ int main(int argc, char **argv) {
     recvbuff_G2 = allocate_on_device<float>(n_elems);
 
     double start_time, end_time;
+    // sync all processors at the end
+    MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
+    if (rank == 0)
+    {
+        start_time = MPI_Wtime();
+    }
     for(int i = 0; i < niter; i++)
     {
         // generate G2 and fill some value in
@@ -54,26 +60,14 @@ int main(int argc, char **argv) {
         int send_tag = 1 + rank;
         send_tag = 1 + MOD(send_tag-1, MPI_TAG_UB); // just to be safe, MPI_TAG_UB is largest tag value
 
-        if (rank == 0)
-        {
-            start_time = MPI_Wtime();
-            printf("start_time: %lf in iteration %d \n", start_time, i);
-        }
-
         for(int icount=0; icount < (mpi_size-1); icount++)
         {
             // encode the originator rank in the message tag as tag = 1 + originator_irank
             int originator_irank = MOD(((rank-1)-icount + 2*mpi_size), mpi_size);
             int recv_tag = 1 + originator_irank;
             recv_tag = 1 + MOD(recv_tag-1, MPI_TAG_UB); // just to be safe, then 1 <= tag <= MPI_TAG_UB
-#ifdef PRINT_DEBUG_INFO
-            printf("rank %d receive G2 [%d] from rank %d in iteration %d \n", rank, recv_tag, left_neighbor, i);
-#endif
-            MPI_CHECK(MPI_Irecv(recvbuff_G2, n_elems, MPI_FLOAT, left_neighbor, recv_tag, MPI_COMM_WORLD, &recv_request));
 
-#ifdef PRINT_DEBUG_INFO
-            printf("rank %d send G2 [%d] to rank %d  in iteration %d \n", rank, send_tag, right_neighbor, i);
-#endif
+            MPI_CHECK(MPI_Irecv(recvbuff_G2, n_elems, MPI_FLOAT, left_neighbor, recv_tag, MPI_COMM_WORLD, &recv_request));
             MPI_CHECK(MPI_Isend(sendbuff_G2, n_elems, MPI_FLOAT, right_neighbor, send_tag, MPI_COMM_WORLD, &send_request));
 
             MPI_CHECK(MPI_Wait(&recv_request, &status));
@@ -85,13 +79,14 @@ int main(int argc, char **argv) {
             CudaMemoryCopy(G2, sendbuff_G2, n_elems);
             send_tag = recv_tag;
         }
+    }
 
-        if (rank == 0)
-        {
-            end_time = MPI_Wtime();
-            printf("end_time: %lf in iteration %d \n", end_time, i);
-            printf("G2 has traveled across the world, time spent: %lf in iteration %d \n", end_time - start_time, i);
-        }
+    if (rank == 0)
+    {
+        end_time = MPI_Wtime();
+        printf("Total time spent on %d iteration: %lf \n", niter, (end_time - start_time));
+        printf("Number of ranks: %d, number of float elements %d \n", mpi_size, n_elems);
+        printf("Average time spent on 1 iteration: %lf \n", (end_time - start_time) / niter);
     }
 
     // sync all processors at the end
