e6da50c4f4aa4c32b47bd1870343949b4e372740
Weile <lokwei9@gmail.com>
2020-04-27 13:48:32 -0400

    adding mpi multi threads support

--- threads_mpi.cpp
+++ threads_mpi.cpp
@@ -22,11 +22,11 @@ void ringG(int thread_id, int rank, int mpi_size, int niter, int n_elems, float*
 
        // get ready for send
       CudaMemoryCopy(sendbuff_G2, G2, n_elems);
-      int send_tag = thread_id;
-      int recv_tag = thread_id;
+      int send_tag = thread_id + 1; // tag is offsets 1 and associated with thread_id
+      int recv_tag = thread_id + 1;
       for(int icount=0; icount < (mpi_size-1); icount++)
       {
-	std::cout << "\nthread " << thread_id <<" from rank " << rank << "\n"; 
+	//std::cout << "\nthread " << thread_id <<" from rank " << rank << "\n"; 
         MPI_CHECK(MPI_Irecv(G2, n_elems, MPI_FLOAT, left_neighbor, recv_tag, MPI_COMM_WORLD, &recv_request));
         MPI_CHECK(MPI_Isend(sendbuff_G2, n_elems, MPI_FLOAT, right_neighbor, send_tag, MPI_COMM_WORLD, &send_request));
         MPI_CHECK(MPI_Wait(&recv_request, &status));
@@ -39,21 +39,27 @@ void ringG(int thread_id, int rank, int mpi_size, int niter, int n_elems, float*
 
 int main(int argc, char **argv)
 {
-  MPI_CHECK(MPI_Init(&argc, &argv));
+  int provided = 0;
+  constexpr int required = MPI_THREAD_FUNNELED;
+  MPI_CHECK(MPI_Init_thread(&argc, &argv, required, &provided));
   int rank, mpi_size;
   MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &mpi_size));
   MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
 
   int n_elems = 10;
-  int n_iter = 1;
-  float* G2 = nullptr;
-  float* sendbuff_G2 = nullptr;
+  int n_iter = 10000;
+  float* G2_first = nullptr;
+  float* G2_second = nullptr;
+  float* sendbuff_G2_first = nullptr;
+  float* sendbuff_G2_second = nullptr;
 
-  G2 = allocate_on_device<float>(n_elems);
-  sendbuff_G2 = allocate_on_device<float>(n_elems);
+  G2_first = allocate_on_device<float>(n_elems);
+  G2_second = allocate_on_device<float>(n_elems);
+  sendbuff_G2_first = allocate_on_device<float>(n_elems);
+  sendbuff_G2_second = allocate_on_device<float>(n_elems);
 
-  std::thread first(ringG, 1, rank, mpi_size, n_iter, n_elems, G2, sendbuff_G2);
-  std::thread second(ringG, 2, rank, mpi_size, n_iter, n_elems, G2, sendbuff_G2);
+  std::thread first(ringG, 1, rank, mpi_size, n_iter, n_elems, G2_first, sendbuff_G2_first);
+  std::thread second(ringG, 2, rank, mpi_size, n_iter, n_elems, G2_second, sendbuff_G2_second);
 
   first.join();
   second.join();
