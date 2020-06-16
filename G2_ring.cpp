0d3bd266206ee32e8cd97576b05c202a671f6f76
Weile Wei <lokwei9@gmail.com>
2020-06-16 16:48:22 -0500

    add is_even to example code

--- G2_ring.cpp
+++ G2_ring.cpp
@@ -30,19 +30,33 @@ int main(int argc, char **argv) {
 
     int left_neighbor = MOD((rank-1 + mpi_size), mpi_size);
     int right_neighbor = MOD((rank+1 + mpi_size), mpi_size);
+    const bool is_even = rank % 2;
 
     // number of G2s
-    int niter = 100;
+    int niter = 1000;
 
     size_t n_elems = 8388608;
+
+    float* G2_h = (float*)malloc(n_elems * sizeof(float));
+    for(int i = 0; i < n_elems; i++)
+    {
+        G2_h[i] = (double) rank;
+    }
+
     float* G2 = nullptr;
     float* G4 = nullptr;
     float* sendbuff_G2 = nullptr;
     float* recvbuff_G2 = nullptr;
 
-    G2 = allocate_on_device<float>(n_elems);
-    sendbuff_G2 = allocate_on_device<float>(n_elems);
-    recvbuff_G2 = allocate_on_device<float>(n_elems);
+    cudaMalloc((void**)&G2, n_elems * sizeof(float));
+    cudaMalloc((void**)&G4, n_elems * sizeof(float));
+    cudaMalloc((void**)&sendbuff_G2, n_elems * sizeof(float));
+    cudaMalloc((void**)&recvbuff_G2, n_elems * sizeof(float));
+
+    cudaMemcpy(G2, G2_h, n_elems * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(G4, G2_h, n_elems * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(sendbuff_G2, G2_h, n_elems * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(recvbuff_G2, G2_h, n_elems * sizeof(float), cudaMemcpyHostToDevice);
 
     double start_time, end_time;
     // sync all processors at the end
@@ -55,14 +69,22 @@ int main(int argc, char **argv) {
     {
         for(int icount=0; icount < (mpi_size-1); icount++)
         {
-            MPI_CHECK(MPI_Irecv(recvbuff_G2, n_elems, MPI_FLOAT, left_neighbor, 1, MPI_COMM_WORLD, &recv_request));
-            MPI_CHECK(MPI_Isend(sendbuff_G2, n_elems, MPI_FLOAT, right_neighbor, 1, MPI_COMM_WORLD, &send_request));
+            if(is_even) {
+                MPI_CHECK(MPI_Irecv(recvbuff_G2, n_elems, MPI_FLOAT, left_neighbor, 1, MPI_COMM_WORLD, &recv_request));
+                MPI_CHECK(MPI_Isend(sendbuff_G2, n_elems, MPI_FLOAT, right_neighbor, 1, MPI_COMM_WORLD, &send_request));
+            }
+            else
+            {
+                MPI_CHECK(MPI_Isend(sendbuff_G2, n_elems, MPI_FLOAT, right_neighbor, 1, MPI_COMM_WORLD, &send_request));
+                MPI_CHECK(MPI_Irecv(recvbuff_G2, n_elems, MPI_FLOAT, left_neighbor, 1, MPI_COMM_WORLD, &recv_request));
+            }
 
             MPI_CHECK(MPI_Wait(&recv_request, &status));
             MPI_CHECK(MPI_Wait(&send_request, &status));
         }
     }
 
+    MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
     if (rank == 0)
     {
         end_time = MPI_Wtime();
